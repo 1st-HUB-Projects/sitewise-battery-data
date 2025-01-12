70db9e01658d896656d78299ef5e2d5d573cfd8c
SAM.AM <samir.amri@gmail.com>
2025-01-12 16:00:42 +0000

    update cets folder

--- virtualiOT_js/singleMQTTMesg.js
+++ virtualiOT_js/singleMQTTMesg.js
@@ -3,9 +3,9 @@ const awsIot = require('aws-iot-device-sdk');
 const ioT_End_Point = "a5b18sm2w1aeo-ats.iot.us-east-1.amazonaws.com"
 const region = "us-east-1"
 const device = awsIot.device({
-    keyPath: './certs/iOTest_PrivateKey.pem',        // Path to your private key
-    certPath: './certs/iOTest_Cert.pem',             // Path to your certificate
-    caPath: './certs/AmazonRootCA1.pem',        // Path to AWS Root CA
+    keyPath: './.certs/iOTest_PrivateKey.pem',        // Path to your private key
+    certPath: './.certs/iOTest_Cert.pem',             // Path to your certificate
+    caPath: './.certs/AmazonRootCA.pem',        // Path to AWS Root CA
     clientId: 'iOTestID',            // Device client ID
     host: `${ioT_End_Point}`  // .iot.${region}.amazonaws.com`   AWS IoT endpoint
 });
